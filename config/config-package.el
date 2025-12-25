bc0088b9d206880355e67f8146ef3ecf9077d373
ygwa <yonggwa@outlook.com>
2025-12-25 15:10:46 +0800

    feat: Configure Projectile to scan only home directory
    Set projectile-project-search-path to scan only the home directory
    and enable projectile-auto-discover for this setting to take effect.

--- config/config-package.el
+++ config/config-package.el
@@ -7,6 +7,8 @@
 (use-package projectile
   :ensure t
   :config
+  (setq projectile-project-search-path '("~/"))
+  (setq projectile-auto-discover t)
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (projectile-mode 1))
